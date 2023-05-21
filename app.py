9205aefbd27405a87d33a79314b6ed893f3225c5
snSon <126743797+snSon@users.noreply.github.com>
2023-05-21 15:58:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ st.header("What is Question?")
 question = st.text_input("write here")
 
 st.subheader("Proof Question")
-proof = st.text_input("proof")
+  proof = st.text_input("proof")
 
 click1 = st.button("1")
 click2 = st.button("2")
 click3 = st.button("3")
+
+if if click1:
+  proof 2 = st.text_input("
