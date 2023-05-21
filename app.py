6e7b6e645cbb6eab4273e486e000854fb612c360
snSon <126743797+snSon@users.noreply.github.com>
2023-05-21 15:53:46 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Proof Math")
+
+st.header("What is Question?")
+question = st.text_input("write here")
+
+st.subheader("Proof Question")
+proof = st.text_input("proof")
+
+click1 = st.button("1")
+click2 = st.button("2")
+click3 = st.button("3")
