ea7862fbf4128dff3e9a40876556f70ccda9146b
snSon <126743797+snSon@users.noreply.github.com>
2023-05-26 17:14:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-@st.cache_data
-def sum(x,y):
-   return x+y
-
 st.title("Proof Math")
 
 with st.container():
@@ -18,13 +14,41 @@ with st.container():
 
 st.write(proof)
 
-click1 = st.button("1")
-click2 = st.button("2")
-click3 = st.button("3")
+def add_symbol(symbol):
+    input_text = st.session_state["input_text"]
+    input_text += symbol
+    st.session_state["input_text"] = input_text
 
-if click1:
-   sub =  st.text_input("calc")
+def main():
+   st.title("Proof Math")
 
-final_proof = st.cache_data(proof, sub)
+   with st.container():
+      st.header("What is Question?")
+      question = st.text_input("write here")
 
-st.write(final_proof)
+      st.write(question)
+
+   with st.container():
+      st.subheader("Proof Question")
+      proof = st.text_input("proof")
+
+      st.write(proof)
+
+   if "input_text" not in st.session_state:
+      st.session_state["input_text"] = ""
+
+          # Symbol buttons
+   st.write("Click a symbol to add:")
+   symbols = ["!", "@", "#", "$", "%"]
+   for symbol in symbols:
+      button_text = f"Add {symbol}"
+      if st.button(button_text):
+         add_symbol(symbol)
+
+          # Input field
+   input_text = st.session_state["input_text"]
+   input_text = st.text_input("Input", input_text, key="input_text")
+   st.write("Input text:", input_text)
+
+if __name__ == "__main__":
+    main()
