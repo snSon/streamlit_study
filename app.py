ed9cffbc374731754c01c7e6760e024e7a6a1c62
snSon <126743797+snSon@users.noreply.github.com>
2023-05-26 17:15:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ with st.container():
 st.write(proof)
 
 def add_symbol(symbol):
-    input_text = st.session_state["input_text"]
-    input_text += symbol
-    st.session_state["input_text"] = input_text
+   input_text = st.session_state["input_text"]
+   input_text += symbol
+   st.session_state["input_text"] = input_text
 
 def main():
    st.title("Proof Math")
@@ -37,7 +37,7 @@ def main():
    if "input_text" not in st.session_state:
       st.session_state["input_text"] = ""
 
-          # Symbol buttons
+      # Symbol buttons
    st.write("Click a symbol to add:")
    symbols = ["!", "@", "#", "$", "%"]
    for symbol in symbols:
@@ -45,10 +45,10 @@ def main():
       if st.button(button_text):
          add_symbol(symbol)
 
-          # Input field
+         # Input field
    input_text = st.session_state["input_text"]
    input_text = st.text_input("Input", input_text, key="input_text")
    st.write("Input text:", input_text)
 
 if __name__ == "__main__":
-    main()
+   main()
