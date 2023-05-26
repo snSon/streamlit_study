c60c7e315d806f02dad9f60469414bc5a18b08d3
snSon <126743797+snSon@users.noreply.github.com>
2023-05-26 17:16:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 import streamlit as st
 
-st.title("Proof Math")
-
-with st.container():
-   st.header("What is Question?")
-   question = st.text_input("write here")
-
-st.write(question)
-
-with st.container():
-   st.subheader("Proof Question")
-   proof = st.text_input("proof")
 
 st.write(proof)
 
