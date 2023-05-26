5ceb63ff64cd1777974aece99cca0aae5c214d6f
snSon <126743797+snSon@users.noreply.github.com>
2023-05-26 17:19:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,18 @@ def main():
     input_text = st.text_input("Input", input_text, key="input_text")
     st.write("Input text:", input_text)
 
+    # 문제 입력
+    st.header("문제 입력")
+    question = st.text_area("문제를 입력하세요")
+
+    # 문제풀이 입력
+    st.header("문제풀이 입력")
+    solution = st.text_area("문제풀이를 입력하세요")
+
+    # 입력받은 문제와 문제풀이 출력
+    st.header("입력받은 문제와 문제풀이")
+    st.write("문제:", question)
+    st.write("문제풀이:", solution)
+
 if __name__ == "__main__":
     main()
