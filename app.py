e4bd6c9f56467a81b766a452e4097b7e49622d22
snSon <126743797+snSon@users.noreply.github.com>
2023-05-27 10:44:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
-def add_symbol(symbol):
-    input_text = st.session_state["input_text"]
+def add_symbol(symbol, input_key):
+    input_text = st.session_state[input_key]
     input_text += symbol
-    st.session_state["input_text"] = input_text
+    st.session_state[input_key] = input_text
 
 def main():
     st.title("Symbol Addition")
 
-    if "input_text" not in st.session_state:
-        st.session_state["input_text"] = ""
+    if "input_text_question" not in st.session_state:
+        st.session_state["input_text_question"] = ""
+
+    if "input_text_solution" not in st.session_state:
+        st.session_state["input_text_solution"] = ""
 
     # Symbol buttons
     st.write("Click a symbol to add:")
@@ -20,17 +23,19 @@ def main():
     for i, symbol in enumerate(symbols):
         button_text = f"Add {symbol}"
         button_col = button_cols[i // 4]  # 열 선택: 4개 버튼마다 열 변경
-        if button_col.button(button_text, key=symbol):  # 각 버튼에 고유한 키(key) 설정
-            add_symbol(symbol)
-            st.text_area("문제풀이를 입력하세요", value=st.session_state["input_text"], height=300, key="solution")  # 문제풀이 입력란에 기호 추가
+
+        with button_col:
+            if st.button(button_text, key=symbol):  # 각 버튼에 고유한 키(key) 설정
+                add_symbol(symbol, "input_text_question")
+                add_symbol(symbol, "input_text_solution")
 
     # 문제 입력
     st.header("문제 입력")
-    question = st.text_area("문제를 입력하세요", height=100)
+    question = st.text_area("문제를 입력하세요", height=100, key="input_text_question")
 
     # 문제풀이 입력
     st.header("문제풀이 입력")
-    solution = st.text_area("문제풀이를 입력하세요", height=300, key="solution")
+    solution = st.text_area("문제풀이를 입력하세요", height=300, key="input_text_solution")
 
     # 백엔드에서 받아온 정답유무와 맞는 풀이 출력
     if st.button("Check Answer"):
