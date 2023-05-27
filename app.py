305691a6a123d301009bff622bc0b198cf0a9f6b
snSon <126743797+snSon@users.noreply.github.com>
2023-05-27 11:17:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,47 @@
 import streamlit as st
 
 def check_answer(solution):
-    # 정답 확인 로직 구현
-    # 예시로 정답을 True로 가정하고 반환
-    is_correct = True
-    correct_solution = "정답"
-    return is_correct, correct_solution
+    # 정답 확인 로직을 구현합니다.
+    # 풀이가 맞으면 True를 반환하고, 틀리면 False를 반환합니다.
+    # 이 예시에서는 정답을 "정답"으로 설정하고 풀이가 "정답"과 일치하면 맞다고 판별합니다.
+    return solution == "정답"
 
-def add_symbol(symbol):
-    input_text_question = st.session_state.get("input_text_question", "")
-    input_text_solution = st.session_state.get("input_text_solution", "")
-    
-    if st.session_state.get("is_question_input", False):
-        input_text_question += symbol
-        st.session_state["input_text_question"] = input_text_question
-    elif st.session_state.get("is_solution_input", False):
-        input_text_solution += symbol
-        st.session_state["input_text_solution"] = input_text_solution
+def add_symbol(symbol, input_text):
+    # 기호를 입력 텍스트에 추가합니다.
+    input_text += symbol
+    return input_text
 
 def main():
-    st.title("고등학교 수학 풀이 검증")
-
-    if "input_text_question" not in st.session_state:
-        st.session_state["input_text_question"] = ""
-        st.session_state["input_text_solution"] = ""
-        st.session_state["is_question_input"] = False
-        st.session_state["is_solution_input"] = False
-
-    # Symbol buttons
-    st.write("Click a symbol to add:")
-    symbols = ["+", "-", "*", "/", "^", "√", "sin", "cos", "tan", "log", "ln", "∫", "dx", "d/dx", "df/dx"]
-    for symbol in symbols:
-        button_text = f"Add {symbol}"
-        if st.button(button_text, key=symbol):
-            add_symbol(symbol)
+    st.title("고등학교 수학 문제 풀이 판별")
 
     # 문제 입력
     st.header("문제 입력")
-    st.session_state["is_question_input"] = True
-    question = st.text_area("문제를 입력하세요", height=100, value=st.session_state["input_text_question"])
-    st.session_state["is_question_input"] = False
+    question = st.text_area("문제를 입력하세요")
 
-    # 문제풀이 입력
-    st.header("문제풀이 입력")
-    st.session_state["is_solution_input"] = True
-    solution = st.text_area("문제풀이를 입력하세요", height=300, value=st.session_state["input_text_solution"])
-    st.session_state["is_solution_input"] = False
+    # 문제 풀이 입력
+    st.header("문제 풀이 입력")
+    solution = st.text_area("문제 풀이를 입력하세요")
+
+    # 기호 지원 버튼
+    st.header("기호 지원")
+    supported_symbols = ["+", "-", "*", "/", "^", "√", "sin", "cos", "tan", "log", "∫"]
+    for symbol in supported_symbols:
+        if st.button(symbol):
+            if st.header("문제 입력").is_running:
+                question = add_symbol(symbol, question)
+            else:
+                solution = add_symbol(symbol, solution)
 
     # 정답 확인
-    if st.button("Check Answer"):
-        is_correct, correct_solution = check_answer(solution)
+    if st.button("풀이 확인"):
+        is_correct = check_answer(solution)
         if is_correct:
-            st.success("Correct!")
+            st.success("정답입니다!")
         else:
-            show_solution = st.checkbox("Show Solution")
-            st.error("Incorrect!")
+            show_solution = st.checkbox("풀이를 확인하시겠습니까?")
+            st.error("틀렸습니다. 다시 확인해보세요.")
             if show_solution:
-                st.info(f"Correct Solution: {correct_solution}")
+                st.info("올바른 풀이: 정답")
 
 if __name__ == "__main__":
     main()
