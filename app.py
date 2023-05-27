30f25ab5455432e350ad61a2c76b538f03f5a642
snSon <126743797+snSon@users.noreply.github.com>
2023-05-27 11:21:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,31 +6,27 @@ def check_answer(solution):
     # 이 예시에서는 정답을 "정답"으로 설정하고 풀이가 "정답"과 일치하면 맞다고 판별합니다.
     return solution == "정답"
 
-def add_symbol(symbol, input_text):
-    # 기호를 입력 텍스트에 추가합니다.
-    input_text += symbol
-    return input_text
-
 def main():
     st.title("고등학교 수학 문제 풀이 판별")
 
     # 문제 입력
     st.header("문제 입력")
-    question = st.text_area("문제를 입력하세요")
+    question = st.text_area("문제를 입력하세요", height=100)
 
     # 문제 풀이 입력
     st.header("문제 풀이 입력")
-    solution = st.text_area("문제 풀이를 입력하세요")
+    solution = st.text_area("문제 풀이를 입력하세요", height=300)
 
-    # 기호 지원 버튼
-    st.header("기호 지원")
-    supported_symbols = ["+", "-", "*", "/", "^", "√", "sin", "cos", "tan", "log", "∫"]
-    for symbol in supported_symbols:
-        if st.button(symbol):
-            if st.header("문제 입력").is_running:
-                question = add_symbol(symbol, question)
-            else:
-                solution = add_symbol(symbol, solution)
+    # 기호 지원
+    st.sidebar.header("기호 지원")
+    symbol = st.sidebar.selectbox("기호를 선택하세요", ("+", "-", "×", "÷", "√", "^", "sin", "cos", "tan", "log", "∫"))
+
+    # 문제나 문제 풀이 입력란에 기호 추가
+    if st.sidebar.button("기호 추가"):
+        if st.text_input == question:
+            question += symbol
+        elif st.text_input == solution:
+            solution += symbol
 
     # 정답 확인
     if st.button("풀이 확인"):
