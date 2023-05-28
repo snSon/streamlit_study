1dcdf45169b737c10f1b46cdc0fd240015018959
snSon <126743797+snSon@users.noreply.github.com>
2023-05-28 17:26:50 +0900

    Update st.py

--- st.py
+++ st.py
@@ -10,8 +10,7 @@ def get_weather(location):
         # API 응답 처리
         weather_data = response.text
         # 날씨 정보 파싱 및 추천 로직 구현
-        recommend_clothing(weather_data)
-        recommend_items(weather_data)
+        return weather_data
     else:
         st.error("날씨 정보를 가져오는데 실패했습니다.")
 
@@ -23,13 +22,21 @@ def recommend_clothing(weather_data):
 def recommend_items(weather_data):
     # 날씨 정보에 따라 필요한 물건을 추천하는 로직을 구현
     # 예시로 간단히 추천 메시지 출력
-    st.write("필요한 물건 추천: 모자와 선크림을 챙기세요.")
+    st.write("챙길 물건 추천: 모자와 선크림을 챙기세요.")
 
 # Streamlit 앱 설정
 st.title("날씨 옷과 물건 추천")
 location = st.text_input("지역을 입력하세요")
-submit_button = st.button("추천")
+
+# 옷 추천 버튼
+recommend_clothing_button = st.button("옷 추천")
+# 챙길 물건 추천 버튼
+recommend_items_button = st.button("챙길 물건 추천")
 
 # 사용자 입력을 받아서 날씨 정보를 조회하고 옷과 필요한 물건을 추천합니다.
-if submit_button:
-    get_weather(location)
+if recommend_clothing_button or recommend_items_button:
+    weather_data = get_weather(location)
+    if recommend_clothing_button:
+        recommend_clothing(weather_data)
+    if recommend_items_button:
+        recommend_items(weather_data)
