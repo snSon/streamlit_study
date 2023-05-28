eeeecfba584ed5c91906e3f7c5716483b3f0ffc3
snSon <126743797+snSon@users.noreply.github.com>
2023-05-28 13:26:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,62 @@
 import streamlit as st
+import requests
+import json
+import base64
 
-def check_answer(solution):
-    # 정답 확인 로직을 구현합니다.
-    # 풀이가 맞으면 True를 반환하고, 틀리면 False를 반환합니다.
-    # 이 예시에서는 정답을 "정답"으로 설정하고 풀이가 "정답"과 일치하면 맞다고 판별합니다.
-    return solution == "정답"
+GITHUB_API_ENDPOINT = "https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+GPT_API_ENDPOINT = "https://api.openai.com/v1/engines/davinci-codex/completions"
+API_KEY = "YOUR_OPENAI_API_KEY"
 
+# GitHub에서 데이터를 가져오는 함수
+def get_data_from_github(owner, repo, path):
+    url = GITHUB_API_ENDPOINT.format(owner=owner, repo=repo, path=path)
+    response = requests.get(url)
+    data = response.json()
+    
+    # Base64로 인코딩된 데이터 디코드
+    content = data["content"]
+    content = content.encode("utf-8")
+    content = base64.b64decode(content)
+    content = content.decode("utf-8")
+
+    return content
+
+# GPT API로부터 답변을 받는 함수
+def get_gpt_answer(question, solution):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "prompt": f"1. 문제: {question}\n2. 문제 풀이: {solution}\n3.",
+        "max_tokens": 100,
+        "temperature": 0.7
+    }
+
+    response = requests.post(GPT_API_ENDPOINT, headers=headers, json=data)
+    answer = response.json()["choices"][0]["text"].strip()
+
+    return answer
+
+# Streamlit 앱 메인 부분
 def main():
     st.title("고등학교 수학 문제 풀이 판별")
 
+    # GitHub에서 데이터 가져오기
+    owner = "your_github_username"
+    repo = "your_github_repo"
+    path = "data.json"
+    data = get_data_from_github(owner, repo, path)
+    data = json.loads(data)
+
     # 문제 입력
     st.header("문제 입력")
-    question = st.text_area("문제를 입력하세요", height=100)
+    question = st.text_area("문제를 입력하세요", height=100, value=data["question"])
 
     # 문제 풀이 입력
     st.header("문제 풀이 입력")
-    solution = st.text_area("문제 풀이를 입력하세요", height=300)
+    solution = st.text_area("문제 풀이를 입력하세요", height=300, value=data["solution"])
 
     # 기호 지원
     st.sidebar.header("기호 지원")
@@ -30,14 +71,20 @@ def main():
 
     # 정답 확인
     if st.button("풀이 확인"):
-        is_correct = check_answer(solution)
-        if is_correct:
-            st.success("정답입니다!")
-        else:
-            st.error("틀렸습니다. 다시 확인해보세요.")
-            show_solution = st.checkbox("풀이를 확인하시겠습니까?")
-            if show_solution:
-                st.info("올바른 풀이: 정답")
+        # GPT 모델로 검증된 데이터 가져오기
+        verified_data = get_gpt_answer(question, solution)
+        verified_data = verified_data.replace("Output:", "").strip()
+
+        # 데이터 업데이트 후 GitHub에 업데이트된 데이터 전송
+        data["question"] = question
+        data["solution"] = solution
+        data["verified_data"] = verified_data
+        updated_data = json.dumps(data)
+        update_data_on_github(owner, repo, path, updated_data)
+
+        # 검증된 데이터 출력
+        st.subheader("검증된 데이터")
+        st.write(verified_data)
 
 if __name__ == "__main__":
     main()
