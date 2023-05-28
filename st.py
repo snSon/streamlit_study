0f081774b656b9538c9a1242e3b5301d809bd2f6
snSon <126743797+snSon@users.noreply.github.com>
2023-05-28 21:03:48 +0900

    Update st.py

--- st.py
+++ st.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-from PIL import Image
 
 def get_weather(location):
     # 기상청 API 호출
@@ -18,32 +17,33 @@ def get_weather(location):
 def recommend_clothing(weather_data):
     # 날씨 정보에 따라 옷을 추천하는 로직을 구현
     # 예시로 간단히 추천 메시지 출력
-    st.write("옷 추천: 편한 반팔과 반바지를 입으세요.")
-    # 추천에 맞는 이미지 보여주기
-    image = Image.open("summer_clothing.jpg")
-    st.image(image, caption="옷 추천 이미지")
+    if "비" in weather_data:
+        st.write("옷 추천: 우비와 장화를 입으세요.")
+    elif "눈" in weather_data:
+        st.write("옷 추천: 패딩과 겨울 장갑을 입으세요.")
+    else:
+        st.write("옷 추천: 편한 반팔과 반바지를 입으세요.")
 
 def recommend_items(weather_data):
     # 날씨 정보에 따라 필요한 물건을 추천하는 로직을 구현
     # 예시로 간단히 추천 메시지 출력
-    st.write("챙길 물건 추천: 모자와 선크림을 챙기세요.")
-    # 추천에 맞는 이미지 보여주기
-    image = Image.open("sunscreen_hat.jpg")
-    st.image(image, caption="물건 추천 이미지")
+    if "비" in weather_data:
+        st.write("챙길 물건 추천: 우산을 가져가세요.")
+    elif "눈" in weather_data:
+        st.write("챙길 물건 추천: 눈덩이 놀이를 즐길 수 있는 장갑을 챙기세요.")
+    else:
+        st.write("챙길 물건 추천: 모자와 선크림을 챙기세요.")
 
 # Streamlit 앱 설정
 st.title("날씨 옷과 물건 추천")
 location = st.text_input("지역을 입력하세요")
-
-# 옷 추천 버튼
-recommend_clothing_button = st.button("옷 추천")
-# 챙길 물건 추천 버튼
-recommend_items_button = st.button("챙길 물건 추천")
+submit_button = st.button("추천")
 
 # 사용자 입력을 받아서 날씨 정보를 조회하고 옷과 필요한 물건을 추천합니다.
-if recommend_clothing_button or recommend_items_button:
+if submit_button:
     weather_data = get_weather(location)
-    if recommend_clothing_button:
+    if weather_data:
         recommend_clothing(weather_data)
-    if recommend_items_button:
         recommend_items(weather_data)
+    else:
+        st.error("날씨 정보를 가져오는데 실패했습니다.")
