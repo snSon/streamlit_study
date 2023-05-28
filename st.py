aad92bb7875c7e235487b5be4b7444ca916f0e31
snSon <126743797+snSon@users.noreply.github.com>
2023-05-28 21:08:48 +0900

    Update st.py

--- st.py
+++ st.py
@@ -15,14 +15,30 @@ def get_weather(location):
         st.error("날씨 정보를 가져오는데 실패했습니다.")
 
 def recommend_clothing(weather_data):
-    # 날씨 정보에 따라 옷을 추천하는 로직을 구현
-    # 예시로 간단히 추천 메시지 출력
-    if "비" in weather_data:
-        st.write("옷 추천: 우비와 장화를 입으세요.")
-    elif "눈" in weather_data:
+    # 날씨 정보에 따라 온도를 추출
+    temperature = extract_temperature(weather_data)
+    if temperature is None:
+        st.error("온도 정보를 가져오는데 실패했습니다.")
+        return
+
+    # 온도에 따라 옷을 추천
+    if temperature < 10:
         st.write("옷 추천: 패딩과 겨울 장갑을 입으세요.")
-    else:
+    elif temperature < 20:
+        st.write("옷 추천: 가벼운 자켓과 스카프를 입으세요.")
+    elif temperature < 30:
         st.write("옷 추천: 편한 반팔과 반바지를 입으세요.")
+    else:
+        st.write("옷 추천: 얇은 옷을 입으세요.")
+
+def extract_temperature(weather_data):
+    # 날씨 정보에서 온도를 추출하는 로직을 구현
+    # 예시로 간단히 온도 정보를 파싱하여 반환
+    temperature = None
+    # 날씨 정보에서 온도를 추출하는 로직 작성
+    # temperature = ...
+
+    return temperature
 
 def recommend_items(weather_data):
     # 날씨 정보에 따라 필요한 물건을 추천하는 로직을 구현
