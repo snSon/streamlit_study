6570f0d380d1f43956847e80197f4df97bc44e48
snSon <126743797+snSon@users.noreply.github.com>
2023-05-29 19:35:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
+font_path = 'NanumBarunGothicLight.ttf'
+font_prop = fm.FontProperties(fname=font_path, size=16)
+
+plt.rcParams['font.family'] = font_prop.get_name()
+
 encodingKey = "ZiLUX%2Bgd1UZWVK6xgqsuh3r7VVxBd33bdidKHPB9pJ2MuoEVMGjgAGms0G4g6PGmLFyVqGhUNP6wivLVImW9hA%3D%3D"
 url = "http://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterList2?"
 params = {'ServiceKey': encodingKey, 'year': '2023', 'type': 'json'}
@@ -65,24 +70,22 @@ try:
             # Display chart for shelter counts
             fig, ax = plt.subplots(figsize=(8, 6))
             plt.bar(shelter_counts.keys(), shelter_counts.values())
-            plt.xlabel("지역")
-            plt.ylabel("수")
-            plt.title("쉼터")
-            plt.xticks(fontname='NanumBarunGothicLight')
-            plt.yticks(fontname='NanumBarunGothicLight')
-            plt.show()
+            plt.xticks(fontproperties=font_prop,size=10)
+            plt.xlabel("지역", fontproperties=font_prop)
+            plt.ylabel("수", fontproperties=font_prop)
+            plt.title("쉼터", fontproperties=font_prop)
+
             st.pyplot(fig)
 
         if st.button("지역별 에어컨 있는 쉼터 갯수"):
             # Display chart for good shelters
             fig, ax = plt.subplots(figsize=(8, 6))
             plt.bar(shelter_good.keys(), shelter_good.values())
-            plt.xlabel("지역")
-            plt.ylabel("수")
-            plt.title("에어컨이 있는 쉼터")
-            plt.xticks(fontname='NanumBarunGothicLight')
-            plt.yticks(fontname='NanumBarunGothicLight')
-            plt.show()
+            plt.xticks(fontproperties=font_prop,size=10)
+            plt.xlabel("지역", fontproperties=font_prop)
+            plt.ylabel("수", fontproperties=font_prop)
+            plt.title("에어컨이 있는 쉼터", fontproperties=font_prop)
+
             st.pyplot(fig)
 
     else:
