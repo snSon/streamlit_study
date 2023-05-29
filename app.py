0be6998ff12534955c04c39acd5911ab8b04746a
snSon <126743797+snSon@users.noreply.github.com>
2023-05-29 16:34:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['font.family'] = 'NanumGothic'
 
 encodingKey = "ZiLUX%2Bgd1UZWVK6xgqsuh3r7VVxBd33bdidKHPB9pJ2MuoEVMGjgAGms0G4g6PGmLFyVqGhUNP6wivLVImW9hA%3D%3D"
 url = "http://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterList2?"
