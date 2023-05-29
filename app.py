04e21a52cbce551fad010e27f2903639cf64d0f7
snSon <126743797+snSon@users.noreply.github.com>
2023-05-29 19:18:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ try:
         if st.button("지역별 무더위 쉼터 갯수"):
             # Display chart for shelter counts
             fig, ax = plt.subplots(figsize=(8, 6))
-            bars = plt.bar(list(shelter_counts.keys()), list(shelter_counts.values()))
+            bars = plt.bar(list(shelter_counts.keys()), list(shelter_counts.values()), color=['red', 'dodgerblue'])
             plt.xlabel("지역", fontproperties=font_prop)
             plt.ylabel("수", fontproperties=font_prop)
             plt.title("쉼터", fontproperties=font_prop)
@@ -77,7 +77,7 @@ try:
         if st.button("지역별 에어컨 있는 쉼터 갯수"):
             # Display chart for good shelters
             fig, ax = plt.subplots(figsize=(8, 6))
-            bars = plt.bar(list(shelter_good.keys()), list(shelter_good.values()))
+            bars = plt.bar(list(shelter_good.keys()), list(shelter_good.values()), color=['red', 'dodgerblue'])
             plt.xlabel("지역", fontproperties=font_prop)
             plt.ylabel("수", fontproperties=font_prop)
             plt.title("에어컨이 있는 쉼터", fontproperties=font_prop)
