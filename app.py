0d431218453c314c43d42a1e61a18c4a8842dd32
snSon <126743797+snSon@users.noreply.github.com>
2023-06-01 18:04:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,108 @@
-import requests
-import json
-import pandas as pd
+from PIL import Image
+from streamlit_option_menu import option_menu
 import streamlit as st
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
+import openai
 
-font_path = 'NanumBarunGothicLight.ttf'
-font_prop = fm.FontProperties(fname=font_path, size=16)
 
-plt.rcParams['font.family'] = font_prop.get_name()
+# 기호 지원
+def add_symbol1(symbol):
+    question = st.session_state["question"]
+    question += symbol
+    st.session_state["question"] = question
 
-encodingKey = "ZiLUX%2Bgd1UZWVK6xgqsuh3r7VVxBd33bdidKHPB9pJ2MuoEVMGjgAGms0G4g6PGmLFyVqGhUNP6wivLVImW9hA%3D%3D"
-url = "http://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterList2?"
-params = {'ServiceKey': encodingKey, 'year': '2023', 'type': 'json', 'pageNo':'1'}
+def add_symbol2(symbol):
+    solution = st.session_state["solution"]
+    solution += symbol
+    st.session_state["solution"] = solution
 
-# API 요청이 성공
-try:
-    response = requests.get(url, params=params)
 
-    if response.status_code == 200:
-        data = response.json()
+API_KEY = "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"
+@st.cache_resource
+def get_openai_chat_completion_model(question, solution):  # type: ignore
+    openai.api_key = API_KEY
+    completion = openai.ChatCompletion.create(  # type: ignore
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "아래에 주어진 question에 대한 solution이 맞는지 틀렸는지 판별해라."},
+            {"role": "user", "content": "solution이 틀렸을 때 '맞겠냐'라고 답해라. solution이 맞았을 때 '좀치네'라고 답해라."},
+            {"role": "user", "content": "solution이 틀렸을 때 '맞겠냐'라고 답하고 '&&&'를 출력하고 올바른 풀이를 제공해라."},
+            {"role": "user", "content": question},
+            {"role": "user", "content": solution}
+        ],
+        temperature=0.8,
+        max_tokens=2048
+    )
+    return completion["choices"][0]["message"]["content"].encode("utf-8").decode()  # type: ignore
 
-        shelters = data['HeatWaveShelter'][1]['row']
+def check_answer(question, solution):  # type: ignore
+    model = get_openai_chat_completion_model(question, solution)  # ChatCompletion 객체 가져오기 #type: ignore
+    return model  # type: ignore
 
-        shelter_counts = {
-            "경기": 0,
-            "서울": 0,
-            "충청남": 0,
-            "충청북": 0,
-            "경상북": 0,
-            "경상남": 0,
-            "전라북": 0,
-            "전라남": 0,
-            "광주": 0,
-        }
 
-        shelter_good = {
-            "경기": 0,
-            "서울": 0,
-            "충청남": 0,
-            "충청북": 0,
-            "경상북": 0,
-            "경상남": 0,
-            "전라북": 0,
-            "전라남": 0,
-            "광주": 0,
-        }
+st.title('수학 증명 계산기')
 
-        # 광역시, 도별 쉼터 개수 계산 및 조건에 맞는 쉼터 저장
-        for shelter in shelters:
-            area_nm = shelter['areaNm']
-            ar = int(shelter['ar'])
-            colr_hold_arcndtn = int(shelter['colrHoldArcndtn'])
+with st.sidebar:
+    selected = option_menu("목록", ["계산기", "계산기 가이드"],
+        icons=['house', 'caret-right-square'], menu_icon="cast", default_index=0)
+    selected
 
-            for area in shelter_counts.keys():
-                if area in area_nm:
-                    if colr_hold_arcndtn == 0:
-                        break
-                    if ar / colr_hold_arcndtn >= 50:
-                        shelter_good[area] += 1
-                    shelter_counts[area] += 1
-                    break
+if selected == '계산기':
 
-        # Streamlit app
-        st.title("지역별 무더위 쉼터 차트")
+    sort = st.radio(
+        "select",
+        ("수학1", "수학2", "미적분", "확률과통계", "벡터")
+    )
 
-        # Create buttons
-        if st.button("지역별 무더위 쉼터 갯수"):
-            # Display chart for shelter counts
-            fig, ax = plt.subplots(figsize=(8, 6))
-            plt.bar(shelter_counts.keys(), shelter_counts.values())
-            plt.xticks(fontproperties=font_prop,size=10)
-            plt.xlabel("지역", fontproperties=font_prop)
-            plt.ylabel("수", fontproperties=font_prop)
-            plt.title("쉼터", fontproperties=font_prop)
+    st.subheader("문제 입력")
 
-            st.pyplot(fig)
+    if "question" not in st.session_state:
+        st.session_state["question"] = ""
 
-        if st.button("지역별 에어컨 있는 쉼터 갯수"):
-            # Display chart for good shelters
-            fig, ax = plt.subplots(figsize=(8, 6))
-            plt.bar(shelter_good.keys(), shelter_good.values())
-            plt.xticks(fontproperties=font_prop,size=10)
-            plt.xlabel("지역", fontproperties=font_prop)
-            plt.ylabel("수", fontproperties=font_prop)
-            plt.title("에어컨이 있는 쉼터", fontproperties=font_prop)
+    # 문제 입력
+    question = st.session_state["question"]
+    question = st.text_input("문제 입력", question)
+    gptQuestion = "question: " + question
 
-            st.pyplot(fig)
 
-    else:
-        st.error("API 요청 실패: " + str(response.status_code))
+    symbol1 = ["√", "^", "sin", "cos", "tan", "log", "∫"]
+    button_col1 = st.columns(5)
+    for i, symbol in enumerate(symbol1):
+        if button_col1[i % 5].button(symbol, key=f"button1_{i}"):
+            add_symbol1(symbol)
 
-#API 요청이 실패
-except requests.RequestException as e:
-    st.error("API 요청 실패: " + str(e))
+
+    st.subheader('문제 풀이 입력')
+
+    if "solution" not in st.session_state:
+        st.session_state["solution"] = ""
+
+    # 문제 풀이 입력
+    solution = st.session_state["solution"]
+    solution = st.text_input("문제풀이 입력", solution)
+    gptSolution = "solution: " + solution
+
+    symbol2 = ["√", "^", "sin", "cos", "tan", "log", "∫"]
+    button_col2 = st.columns(5)
+    for i, symbol in enumerate(symbol2):
+        if button_col2[i % 5].button(symbol, key=f"button2_{i}"):
+            add_symbol2(symbol)
+
+
+    st.subheader("정답 확인")
+
+    is_correct = check_answer(gptQuestion, gptSolution)
+    sub = is_correct.split("&&&")
+
+    if st.button("OX 확인"):
+        if question and solution:  # type: ignore
+            st.write(is_correct)
+            if "좀치네" in is_correct:
+                st.success("정답입니다!")
+            elif "맞겠냐" in is_correct:
+                st.error("틀렸습니다. 다시 확인해보세요.")
+            else:
+                st.write("멍청한 gpt")
+
+    show_solution = st.checkbox("풀이를 확인하시겠습니까?")
+    if show_solution:
+        st.write(sub[1])
